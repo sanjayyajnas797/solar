eda4f4edfe0219bfcb54689c49da8d2171621a0f
Sanjay Yajnas <your_email@example.com>
2026-03-03 17:11:15 +0530

    updated backend url

--- frontend/Deye_cloud/src/pages/config.jsx
+++ frontend/Deye_cloud/src/pages/config.jsx
@@ -2,5 +2,6 @@
 
 // CURRENT → LOCAL BACKEND
 // const API_BASE = "http://13.203.219.55:5000/api";
-const API_BASE="http://localhost:5000/api";
+// const API_BASE="http://localhost:5000/api";
+const API_BASE="https://solar-backend.onrender.com/api";
 export default API_BASE;
